7d2ea58deea99794dc00685b0a216ba40ff17765
wilmar.rincon <wilmar.rincon@linktic.com>
2020-02-28 12:18:48 -0500

    componentes

---
+++ src/componetes/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Card extends React.Component {
+  render () {
+      return  (
+          <div>
+              <div>
+                  <img />
+              </div>
+              <div>
+                  <h1>dfsfsfsfsdfsdf</h1>
+              </div>
+          </div>
+      )
+  }
+}
+
+export default Card 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Card from './componetes/Card'
 
 
-function name(user) {
-    if (user) {
-        return <h1>dsfsdfdf</h1>
-    } else {
-        return <h1>llllllll</h1>
-    }
-}
-
-const element = <h1>prueba {name(user)} </h1>
 const container = document.getElementById('root')
 
-ReactDOM.render(element, container)
+ReactDOM.render(<Card></Card>, container)
 
 
 
